eb13949136f8dd2618fec03e07230c78446009ee
Levy Kariuki <levthigkari@gmail.com>
2020-02-24 10:41:38 +0300

    Added form function to js

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,18 @@ $(document).ready(function(){
       });
       });
       event.preventDefault();
-    
+      
+      $('form#myForm').submit(function(){
+        var name = $('#name').val();
+        var pass = $('#email').val();
+        var mess = $('#mess').val();
+        alert("Hi " +name+ " we have received your message and we will get in touch. Thank you for contacting us.");
+      });
+      
+      $("button").on('click', function(){
+        $('form').each(function(){
+          this.reset();
+        });
+      
+  });
+  });
